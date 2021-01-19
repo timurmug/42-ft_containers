23f2befdf2811bce70c633cf8d42a1feab12afe6
Quaro Tamarillo <qtamaril@un-j4.kzn.21-school.ru>
2021-01-19 13:27:16 +0300

    cmake folder deleted

--- cmake-build-debug/unit_tests/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/qtamaril/Desktop/qtamaril/my_git/42-ft_containers/unit_tests/googletest/googletest
-# Build directory: /Users/qtamaril/Desktop/qtamaril/my_git/42-ft_containers/cmake-build-debug/unit_tests/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
