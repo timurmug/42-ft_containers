8d039fd6dc886642a6f9c7b215c937b3899c0615
Quaro Tamarillo <qtamaril@si-g1.kzn.21-school.ru>
2021-01-25 11:56:36 +0300

    empty map and test files

---
+++ includes/map.hpp
@@ -0,0 +1,14 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <memory>
+
+template < class Key,                                               // map::key_type
+        class T,                                                    // map::mapped_type
+        class Compare = std::less<Key>,                             // map::key_compare
+        class Alloc = std::allocator<std::pair<const Key,T> > >     // map::allocator_type
+class map {
+
+};
+
+#endif
---
+++ unit_tests/MapTests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Quaro Tamarillo on 1/25/21.
+//
+
+#include "map.hpp"
+#include "gtest/gtest.h"
+#include <map>
+
+TEST(Map_Base, empty_constructor) {
+    FAIL();
+}
+
